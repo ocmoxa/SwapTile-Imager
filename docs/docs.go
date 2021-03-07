894c594ccace1935cdc16a1a6648602080a2605e
Krivchun Maxim <hedhyw@yahoo.com>
2021-03-07 11:36:52 +0300

    Cosmetic change

--- docs/docs.go
+++ docs/docs.go
@@ -6,6 +6,8 @@ import "embed"
 //go:embed swagger.yml ui
 var embededFiles embed.FS
 
+// Swagger returns global file system with swagger files.
+// It includes the file ./swagger.yml and the folter ./ui.
 func Swagger() embed.FS {
 	return embededFiles
 }
